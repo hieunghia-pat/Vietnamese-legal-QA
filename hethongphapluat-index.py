035d7c2fb7f4c07cf8990cf6419b90ecb53df890
hieunghia-pat <19520178@gm.uit.edu.vn>
2023-07-13 10:31:11 +0700

    crawling hethongphapluat in parallel approach

--- hethongphapluat-index.py
+++ hethongphapluat-index.py
@@ -7,6 +7,8 @@ import os
 import re
 import argparse
 import docx
+import threading
+import multiprocessing
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--from-page", type=int, required=True)
@@ -17,8 +19,6 @@ MAX_PAGE = 43_035
 FROM_PAGE = args.from_page
 TO_PAGE = args.to_page
 domain = "https://hethongphapluat.com"
-parsed_link = set()
-data = []
 
 def parsing_docx_file(link: str) -> list:
     docx_stream = requests.get(link, stream=True)
@@ -39,69 +39,68 @@ def save_docx(link: str, docx_id: str) -> None:
     with open(os.path.join("data", f"{docx_id}.docx"), "wb") as docx_file:
         docx_file.write(docx_stream.content)
 
-for page in range(FROM_PAGE, TO_PAGE):
+def start_crawling_page(page_id):
     url = os.path.join(domain, f"thu-vien-ban-an_page-{page}.html")
     
     main_soup = BeautifulSoup(requests.get(url).content, features="lxml")
+
+    print(url)
+
+    threads = []
+    for div in main_soup.find_all('div', class_="list-body"):
+        link = div.a["href"]
+        threads.append(threading.Thread(
+            target=start_crawling_docx,
+            args=(link, )
+        ))
+
+    for thread in threads:
+        thread.join()
+
+def start_crawling_docx(link):
+    session = requests.Session()   
     while True:
         try:
-            session = requests.Session()
+            content = session.get(link).content
+            break
+        except:
+            continue
+    soup = BeautifulSoup(content, features="lxml")
+
+    try:
+        download_section = soup.find("div", {"class": "download-section"})
+        button = download_section.find_all("button")[1]
+    except:
+        print("Cannot find download section for ", link)
+        return
+
+    link_to_docx = button.a["href"]
+    if link_to_docx == "":
+        print("There is not docx file available ")
+        return
+
+    while True:
+        try:
+            ba_info = soup.find("div", {"class": "ba-info"})
+            li = ba_info.ul.find_all("li")[0]
+            li_text = li.text
+            li_text = re.sub("Số bản án:", "", li_text).strip()
+            save_docx(link_to_docx, li_text)
             break
         except:
             continue
 
-    print(url)
+processes = []
+for page in range(FROM_PAGE, TO_PAGE):
+    processes.append(multiprocessing.Process(
+        target=start_crawling_page, 
+        args=(page, )
+    ))
 
-    for div in main_soup.find_all('div', class_="list-body"):
-        link = div.a["href"]
-        title = div.a["title"]
+for process in processes:
+    process.join()
 
-        title = " ".join(title.split()[:5])
-        
-        if link in parsed_link:
-            continue
-
-        link = os.path.join(domain, link)
-        while True:
-            try:
-                content = session.get(link).content
-                break
-            except:
-                continue
-        soup = BeautifulSoup(content, features="lxml")
-
-        item = {}
-
-        try:
-            download_section = soup.find("div", {"class": "download-section"})
-            button = download_section.find_all("button")[1]
-        except:
-            print("Cannot find download section for ", link)
-            continue
-
-        link_to_docx = button.a["href"]
-        if link_to_docx == "":
-            continue
-        # paragraphs = parsing_docx_file(link_to_docx)
-        # item["paragraphs"] = paragraphs
-        
-        right_ba_info = soup.find("div", {"id": "right-ba-info"})
-        item["info"] = right_ba_info.h1.text
-
-        while True:
-            try:
-                ba_info = soup.find("div", {"class": "ba-info"})
-                li = ba_info.ul.find_all("li")[0]
-                li_text = li.text
-                li_text = re.sub("Số bản án:", "", li_text).strip()
-                item["number"] = li_text
-                save_docx(link_to_docx, li_text)
-                break
-            except:
-                continue
-
-
-        data.append(item)
-
-json.dump(data, open(f"hethongphapluat-banan_from-{FROM_PAGE}_to-{TO_PAGE}.json", "w+"), ensure_ascii=False, indent=4)
 print("Done")
+
+if __name__ == "__main__":
+    pass
